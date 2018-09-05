7697af0b95c7c34ab6f1e383d7f0268100554aa3
fukami <fukami@3films.com>
2018-09-06 01:53:46 +0900

    first commit jquery-qrcode

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#qrcode').qrcode({
+        text: 'hogehoge',
+        width: 200,
+        height: 200,
+        colorDark : "#000000",
+        colorLight : "#ffffff"
+    });
+});
+
